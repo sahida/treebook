1a4033f27548953b49346e3fb84325f986e73e08
Damanjeet Sahi <dsahi1991@gmail.com>
2013-09-09 23:04:07 -0400

    Added simple_form, updated several forms to bootstrap/simple_form. Added login/logout/register/profile links to our nav. Added collection select to our status form.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,8 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :name
+	#user_id is not native
+  attr_accessible :content, :user_id 
+  	#user is native to ruby and can recognize user_id
+  	#Allows us retrieve a user for a given status (status.user). Then we can use ser methods on this user obj to return the name of the user!
+  	#A Status belongs_to only one user. Allows us to retrieve a status that belongs to a single user.
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
+  # Setup accessible (or protected) attributes for your model (IN DATABASE)
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
+
+  #The opposite of belongs_to. This says that a User has many statuses. This links multiple statuses to a user.
+  has_many :statuses
+
+  def full_name
+  	first_name + " " + last_name
+  end
 end
