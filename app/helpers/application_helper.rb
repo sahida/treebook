7bb22ada762af58d138d459800064ee6f6c81e93
Damanjeet Sahi <dsahi1991@gmail.com>
2013-09-13 19:50:25 -0400

    Styled the flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	# Helper function that adds the class to notice/alert flash messages for proper alert colors.
+	def flash_class (type)
+		case type
+		when :alert
+			 "alert-warning"
+		when :notice
+			 "alert-success"
+		else
+			 "alert-warning"
+		end
+	end
 end
